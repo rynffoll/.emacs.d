8e3a3c9ad8b551d6f13b167d7f7bc817480c357a
Ruslan Kamashev <rynffoll@gmail.com>
2018-10-21 22:35:57 +0300

    Add eyebrowse

--- init.el
+++ init.el
@@ -304,6 +304,34 @@
   :config
   (winum-mode))
 
+(use-package eyebrowse
+  :defer 1
+  :general
+  (my-leader-def
+    "w" '(:ignore t :which-key "Workspaces")
+    "wk" 'eyebrowse-close-window-config
+    "w TAB" 'eyebrowse-last-window-config
+    "wR" 'eyebrowse-rename-window-config
+    "ww" 'eyebrowse-switch-to-window-config
+    "w0" 'eyebrowse-switch-to-window-config-0
+    "w1" 'eyebrowse-switch-to-window-config-1
+    "w2" 'eyebrowse-switch-to-window-config-2
+    "w3" 'eyebrowse-switch-to-window-config-3
+    "w4" 'eyebrowse-switch-to-window-config-4
+    "w5" 'eyebrowse-switch-to-window-config-5
+    "w6" 'eyebrowse-switch-to-window-config-6
+    "w7" 'eyebrowse-switch-to-window-config-7
+    "w8" 'eyebrowse-switch-to-window-config-8
+    "w9" 'eyebrowse-switch-to-window-config-9
+    "w[" 'eyebrowse-prev-window-config
+    "w]" 'eyebrowse-next-window-config
+    "wn" 'eyebrowse-create-window-config)
+  :custom
+  (eyebrowse-new-workspace t "Clean up and display the scratch buffer")
+  (eyebrowse-wrap-around t)
+  :config
+  (eyebrowse-mode t))
+
 (use-package cus-edit
   :ensure nil
   :custom
