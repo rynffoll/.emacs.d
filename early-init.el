ecb44afed8ef8b6955448dc625cd11350fad3eff
Ruslan Kamashev <rynffoll@gmail.com>
2020-01-19 22:10:49 +0300

    Move early-init.el to config.org

--- early-init.el
+++ early-init.el
@@ -1,9 +1,7 @@
 ;;; -*- lexical-binding: t; -*-
 
-;; Package Manager
 (setq package-enable-at-startup nil)
 
-;; GC tweaks
 (setq gc-cons-threshold most-positive-fixnum
       gc-cons-percentage 0.6)
 
@@ -21,7 +19,6 @@
                        (format "%.2fs" (float-time gc-elapsed))
                        gcs-done)))
 
-;; File handler tweaks
 (defvar my--file-name-handler-alist file-name-handler-alist)
 (setq file-name-handler-alist nil)
 
@@ -29,7 +26,6 @@
           #'(lambda ()
               (setq file-name-handler-alist my--file-name-handler-alist)))
 
-;; UI
 (add-to-list 'default-frame-alist '(left . 0.5))
 (add-to-list 'default-frame-alist '(top . 0.5))
 (add-to-list 'default-frame-alist '(width . 0.75))
@@ -39,10 +35,5 @@
 (add-to-list 'default-frame-alist '(vertical-scroll-bars))
 (add-to-list 'default-frame-alist '(vertical-scroll-bars))
 (add-to-list 'default-frame-alist '(font . "Fira Mono Medium 14"))
-;; (add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
-;; (add-to-list 'default-frame-alist '(ns-appearance . light))
 
-;; Resizing the Emacs frame can be a terribly expensive part of changing the
-;; font. By inhibiting this, we easily halve startup times with fonts that are
-;; larger than the system default.
 (setq frame-inhibit-implied-resize t)
