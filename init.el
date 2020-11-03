726adceec99a230d200b5446137d42ced00ef6ee
Ruslan Kamashev <rynffoll@gmail.com>
2020-11-03 14:32:13 +0300

    Enable magit-todos

--- init.el
+++ init.el
@@ -1271,7 +1271,6 @@
                                   (,magit-clone-default-directory . 1))))
 
 (use-package magit-todos
-  :disabled
   :custom
   (magit-todos-keyword-suffix (rx (optional "(" (1+ (not (any ")"))) ")" ":")))
   :hook
