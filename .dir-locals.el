a6345ac5a837c4ddb404aa8c81ad517152bb7ff0
Ruslan Kamashev <rynffoll@gmail.com>
2024-12-31 18:09:43 +0400

    Enable copilot-mode for config.org

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,7 +1,5 @@
-;;; Directory Local Variables
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((magit-status-mode
-  (magit-todos-exclude-globs . (".git/"
-				"init.el"
-				"early-init.el"))))
+((magit-status-mode . ((magit-todos-exclude-globs . (".git/" "init.el" "early-init.el"))))
+ (org-mode . ((eval . (copilot-mode)))))
